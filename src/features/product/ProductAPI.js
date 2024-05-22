7d324e7203ecba40154bc87d1b343e599a491ba4
Akshamalik <aksha5u786@gmail.com>
2024-05-22 10:49:06 +0530

    added filters and sort functionalities

--- src/features/product/ProductAPI.js
+++ src/features/product/ProductAPI.js
@@ -1,8 +1,35 @@
-export function fetchAllProducts(){
-return new Promise(async (resolve) => {
-  const response=await fetch('https://localhost:8080/products')
-  const data=await response.json();
-  resolve({data})
+export function fetchAllProducts() {
+  return new Promise(async (resolve) => {
+    //TODO: we will not hard-code server URL here
+    const response = await fetch("http://localhost:8080/products");
+    const data = await response.json();
+    resolve({ data });
+  });
 }
-);
+
+export function fetchProductsByFilters(filter,sort) {
+  // filter = {"category":["smartphone,"laptops"]}
+  //sort ={_sort:"price",_roder="desc"}
+  // TODO : on server we will support multi values
+  let queryString = "";
+  for (let key in filter) {
+    const categoryValues=filter[key]
+    if(categoryValues.length>1){
+      const lastCategoryValue=categoryValues[categoryValues.length-1]
+      queryString += `${key}=${filter[lastCategoryValue]}&`;
+    }
+    
+  }
+  for(let key in sort){
+    queryString+=`${key}=${sort[key]}&`
+  }
+
+  return new Promise(async (resolve) => {
+    //TODO: we will not hard-code server URL here
+    const response = await fetch(
+      "http://localhost:8080/products?" + queryString
+    );
+    const data = await response.json();
+    resolve({ data });
+  });
 }
